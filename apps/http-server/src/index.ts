f7a3271c0178a9ac4e38e74523419e799e28d492
Moksh <moksh8516@gmail.com>
2025-04-07 13:15:07 +0530

    update packages

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import {JWT_SECRET} from "@drawapp/backend-common/config";
 const app = express();
+
 app.get("/",(req:Request, res:Response)=>{
   res.send("Hello World!"+JWT_SECRET);
 })
